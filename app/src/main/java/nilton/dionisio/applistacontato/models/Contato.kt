fa3af4609570ef465a2d273d2b5b9bbb997b6938
Laboratórios - W10 - Fatec Mauá <labs@labtec.local>
2023-09-02 11:24:09 -0300

    Initial commit

---
+++ app/src/main/java/nilton/dionisio/applistacontato/models/Contato.kt
@@ -0,0 +1,12 @@
+package nilton.dionisio.applistacontato.models
+
+
+//o var permite apenas a leitura,não a edição
+class Contato (
+    var nome:String,
+    var email:String,
+    var telefone:String,
+    var endereco:String,
+    var foto:String
+
+)
